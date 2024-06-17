3e434fdded3fe3f49d2c202944b0357e3ba6923b
Artyom Bely <69087978+ArtemWhite@users.noreply.github.com>
2024-06-17 11:47:00 +0300

    Add files via upload

---
+++ диспетчер задач/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lBox = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.tBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lBox
+            // 
+            this.lBox.FormattingEnabled = true;
+            this.lBox.Location = new System.Drawing.Point(12, 12);
+            this.lBox.Name = "lBox";
+            this.lBox.Size = new System.Drawing.Size(246, 420);
+            this.lBox.TabIndex = 0;
+            this.lBox.SelectedIndexChanged += new System.EventHandler(this.lBox_SelectedItemChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(264, 351);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(278, 81);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Снять задачу";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // tBox
+            // 
+            this.tBox.Location = new System.Drawing.Point(268, 12);
+            this.tBox.Name = "tBox";
+            this.tBox.Size = new System.Drawing.Size(274, 20);
+            this.tBox.TabIndex = 3;
+            this.tBox.TextChanged += new System.EventHandler(this.tBox_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(268, 49);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(274, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Открыть";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tBox2
+            // 
+            this.tBox2.Location = new System.Drawing.Point(264, 312);
+            this.tBox2.Name = "tBox2";
+            this.tBox2.Size = new System.Drawing.Size(274, 20);
+            this.tBox2.TabIndex = 5;
+            this.tBox2.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(570, 450);
+            this.Controls.Add(this.tBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tBox);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.lBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lBox;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox tBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox tBox2;
+    }
+}
+
---
+++ диспетчер задач/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Threading;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            foreach (var item in Process.GetProcesses())
+                lBox.Items.Add(item.ProcessName);
+            //TimerCallback tcb = new TimerCallback(update);
+            //System.Threading.Timer timer = new System.Threading.Timer(tcb, 0,0,1000);
+            Thread ts = new Thread(update);
+            ts.IsBackground = true;
+            ts.Start();
+        }
+        void update()
+        {
+            while (true)
+            {
+                lBox.Items.Clear();
+                foreach (var item in Process.GetProcesses())
+                    lBox.Items.Add(item.ProcessName);
+                Thread.Sleep(5000);
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+ 
+            foreach (var p in Process.GetProcessesByName($"{tBox2.Text}"))
+                p.CloseMainWindow();
+            lBox.Items.Remove(tBox2.Text);
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Process p = new Process();
+            p.StartInfo = new ProcessStartInfo($"{tBox.Text}") ;
+            p.Start();
+            lBox.Items.Add($"{tBox.Text}");
+            
+
+        }
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            lBox.Items.Clear();
+            var list = Process.GetProcesses();
+            
+                foreach (var item in list)
+                {
+                    if (item.ProcessName.ToLower().Contains(tBox2.Text.ToLower()))
+                        lBox.Items.Add(item.ProcessName);
+
+                }
+            
+            
+        }
+        private void tBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void lBox_SelectedItemChanged(object sender, EventArgs e)
+        {
+            tBox2.Text = lBox.SelectedItem.ToString();
+        }
+    }
+}
